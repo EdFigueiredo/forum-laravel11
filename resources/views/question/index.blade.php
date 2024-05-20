6275f0aedb1a486de6b65189619bbc144d6b0228
LuanMartins <luan_18martins@hotmail.com>
2024-05-20 18:39:24 -0300

    refactor: add new layout for index question

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,41 +1,42 @@
-<h1>Listagem de perguntas</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Perguntas') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-4">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">Assunto</th>
+                                <th scope="col" class="px-6 py-3">Conteúdo</th>
+                                <th scope="col" class="px-6 py-3">Categoria</th>
+                                <th scope="col" class="px-6 py-3">Ações</th>
+                            </tr>
+                    </thead>
+                    <tbody class="text-gray-600 dark:text-white text-sm font-light">
+                        @forelse ($questions as $question)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-70">
+                                <td class="px-6 py-4">{{ $question->subject }}</td>
+                                <td class="px-6 py-4">{{ $question->text }}</td>
+                                <td class="px-6 py-4">{{ $question->category->name }}</td>
+                                <td class="px-6 py-4 flex justify-center space-x-2">
+                                    <a href="{{ route('questions.show', $question->id) }}">
+                                        <i class="fa-solid fa-building"></i>
+                                    </a>
+                                    <a href="{{ route('questions.edit', $question->id) }}">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                </td>
+                            </tr>    
+                        @empty
 
-<a href="{{ route('questions.create') }}"> Cadastrar Perguntas</a>
-
-<br>
-<br>
-
-@if(session('success'))
-    <p>{{ session('success') }}</p>
-@endif
-
-<table>
-    <thead>
-        <tr>
-            <th>Assunto</th>
-            <th>Conteúdo</th>
-            <th>Categoria</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($questions as $question)
-            <tr>
-                <td>{{ $question->subject }}</td>
-                <td>{{ $question->text }}</td>
-                <td>{{ $question->category->name }}</td>
-                <td>
-                    <a href="{{ route('questions.show', $question->id) }}">Visualizar</a>
-                    <a href="{{ route('questions.edit', $question->id) }}">Editar</a>
-                    
-                </td>
-            </tr>
-        @empty
-            <tr>
-                <td colspan="2">
-                    Não existem perguntas cadastradas
-                </td>
-            </tr>
-        @endforelse
-    </tbody>
-</table>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
