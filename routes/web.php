4068feeaeee172382443569bfe6bce08ecdaf519
LuanMartins <luan_18martins@hotmail.com>
2024-06-01 10:18:31 -0300

    refactor: add route get forum controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ForumController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/questions', QuestionController::class);
+
+    Route::get('/forum', [ForumController::class, 'index'])->name('forum.index');
 });
 
 require __DIR__.'/auth.php';
