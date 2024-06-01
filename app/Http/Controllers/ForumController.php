2526bb9c1ccf77b1444683c5f53d8f469affdb78
LuanMartins <luan_18martins@hotmail.com>
2024-06-01 11:46:30 -0300

    refactor: add relation and create new function reply

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreReplyQuestion;
 use App\Models\Question;
+use App\Models\ReplyQuestion;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ForumController extends Controller
 {
@@ -16,10 +19,25 @@ class ForumController extends Controller
 
     public function show(string $id)
     {
-        if(! $question = Question::find($id)){
+        if(! $question = Question::with(['user','replies'])->find($id)){
             abort(404);
         }
 
         return view('forum.show', compact('question'));
+    }
+    
+
+    public function reply(StoreReplyQuestion $request)
+    {
+        if(! $question = Question::find($request->question_id)){
+            abort(404);
+        }
+
+        ReplyQuestion::create([
+            'user_id' => Auth::id(),
+            ...$request->validated()
+        ]);
+
+        return redirect()->back()->with('success', 'Pergunta respondida com sucesso!');
     }
 }
